a2f6bef53a3815f1cc7f141068294e8664691c9e
donrestarone <shashikejayatunge@gmail.com>
2018-03-21 14:59:40 -0400

    finish exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,22 @@
+fav_colors = ["black", "pink", "blue"]
+puts "last element in the favorite color array is #{fav_colors.last}"
+
+
+around_world = {"country1" => 199599383, "country2" => 1996343434, "country3" => 199735354}
+
+#add country 4 to hash
+around_world["country4"] = 33332
+puts "around world hash has #{around_world}"
+
+
+coin_flip = ["heads", "heads", "heads", "tails", "tails"]
+puts "reversed array for coin flip #{coin_flip.reverse}"
+
+#print population of country 2 in the hash
+puts "one of the cities in the around world array has #{around_world["country2"]} people"
+
+fav_artists = ["Black Sabbath", "Pink Floyd", "Queen"]
+
+puts "i think #{fav_artists[2]} is loud "
+puts "i think #{fav_artists[0]} is louder "
+puts "i think #{fav_artists[2]} is great "
