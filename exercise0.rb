c820d4e4ab079549fc753be31814fbf49fd6ff24
donrestarone <shashikejayatunge@gmail.com>
2018-03-21 14:26:21 -0400

    complete exercise0

--- exercise0.rb
+++ exercise0.rb
@@ -7,4 +7,7 @@
 
 
 #hashes
-	
+	word_definitions = {"stuff" => "things", "do" => "action", "dont" => "inaction"}
+	fav_movies = {"movie1" => 1995, "movie2" => 1996, "movie3" => 1997}
+	around_world = {"country1" => 199599383, "country2" => 1996343434, "country3" => 199735354}
+	names_ages = {"buddy" => 19, "guy" => 34, "pal" => 54}
