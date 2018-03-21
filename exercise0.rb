6d97764eaaa1670b307ffad0533dc3127f509f28
donrestarone <shashikejayatunge@gmail.com>
2018-03-21 14:20:28 -0400

    complete arrays

---
+++ exercise0.rb
@@ -0,0 +1,10 @@
+#arrays 
+	fav_colors = ["black", "pink", "blue"]
+	ages_everyone = [23, 22, 24]
+	coin_flip = ["heads", "heads", "heads", "tails", "tails"]
+	fav_artists = ["Black Sabbath", "Pink Floyd", "Queen"]
+	fav_c_sym = [:black, :pink, :blue]
+
+
+#hashes
+	
