0d290dd99857373d4d7f52f4d942bdcb0c273de4
donrestarone <shashikejayatunge@gmail.com>
2018-03-21 17:33:41 -0400

    finish exercise 6

---
+++ exercise6.rb
@@ -0,0 +1,12 @@
+movie_list = {"The Matrix" => 1999, "Avatar" => 2009, "Star Wars Episode 9" => 2019}
+
+#array within an array
+button_rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ["*", 0, "#"]]
+
+#hash within an array
+#hashes are using symbols ":""
+country_1 = {name: "USA", continent: "North America", island: false}
+country_2 = {name: "Canada", continent: "North America", island: false}
+country_3 = {name: "Madagascar", continent: "Asia", island: true}
+
+countries = [country_1, country_2, country_3]
